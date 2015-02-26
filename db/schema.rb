b00c06180a513805ec9f29b0275b787b2de526f2
MGat22 <zanku2244@yahoo.com>
2015-02-26 15:38:20 -0500

    pushing changes to users, added admin, minor user privileges

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150223224502) do
+ActiveRecord::Schema.define(version: 20150225225920) do
 
   create_table "posts", force: :cascade do |t|
     t.string   "title"
@@ -29,6 +29,10 @@ ActiveRecord::Schema.define(version: 20150223224502) do
     t.string   "password_digest"
     t.datetime "created_at",      null: false
     t.datetime "updated_at",      null: false
+    t.boolean  "admin"
+    t.string   "username"
   end
+
+  add_index "users", ["username"], name: "index_users_on_username", unique: true
 
 end
