788e5047efcf798ca55d34dd30e828ba22c4e47f
MGat22 <zanku2244@yahoo.com>
2015-02-26 15:25:59 -0500

    user file changes

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,12 @@
 class User < ActiveRecord::Base
 	has_secure_password
 	has_many :posts
+
+	validates :name, :email, uniqueness: true, presence: true
+	validates :username, presence: true, uniqueness: true
+    	validates :email, uniqueness: true, presence: true, format: { with: /\A[^@]+@[^@]+\z/, message: 'looks jacked-up. Are you sure this is right?' }
+
+	def is_admin?
+		self.admin
+	end
 end
