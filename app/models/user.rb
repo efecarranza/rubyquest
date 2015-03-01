22396e837a951f8915996d944b8a6df742a7e1e5
piscu Carranza <fcarr004@fiu.edu>
2015-03-01 14:24:12 -0500

    Added dev stuff to site, now working. Need to add options by is admin

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,7 +4,7 @@ class User < ActiveRecord::Base
 
 	validates :name
 	validates :username, presence: true, uniqueness: true
-    validates :email, uniqueness: true, presence: true, format: { with: /\A[^@]+@[^@]+\z/, message: 'Invalid e-mail. Please make sure your e-mail is in the format: name@example.com' }
+  validates :email, uniqueness: true, presence: true, format: { with: /\A[^@]+@[^@]+\z/, message: 'Invalid e-mail. Please make sure your e-mail is in the format: name@example.com' }
 
 	def is_admin?
 		self.admin
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150225225920) do
+ActiveRecord::Schema.define(version: 20150301181902) do
+
+  create_table "articles", force: :cascade do |t|
+    t.string   "title"
+    t.text     "text"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "posts", force: :cascade do |t|
     t.string   "title"
